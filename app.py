dc55a5e107712d746241fbe8d7bf2c2dd1d1a94a
GabryelVenzel <gabryelvenzel@gmail.com>
2025-08-11 18:10:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -299,11 +299,8 @@ with abas[0]:
                 st.error("❌ O cálculo não convergiu. Verifique os dados de entrada.")
     
     st.markdown("---")
-    st.markdown("""
-    > **Observação:** Emissividade de 0.9 considerada no cálculo.
-    st.markdown("---")
-    > **Nota:** Os cálculos são realizados de acordo com a norma ASTM C680.
-    """)
+    st.markdown(""" > **Observação:** Emissividade de 0.9 considerada no cálculo.""")
+    st.markdown("""> **Nota:** Os cálculos são realizados de acordo com a norma ASTM C680.""")
 
 with abas[1]:
     st.subheader("Cálculo de Espessura Mínima para Evitar Condensação")
@@ -333,4 +330,5 @@ with abas[1]:
                 st.success(f"✅ Espessura mínima para evitar condensação: {espessura_final * 1000:.1f} mm".replace('.',','))
             else:
                 st.error("❌ Não foi possível encontrar uma espessura que evite condensação até 500 mm.")
+
 
